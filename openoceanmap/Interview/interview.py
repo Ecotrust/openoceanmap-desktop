a9c004d8eadf8a547a2d4ff99e3e95afa9575dc5
Aaron Racicot <aaronr@z-pulley.com>
2007-08-23 16:43:22 +0000

    new interview class

---
+++ openoceanmap/Interview/interview.py
@@ -0,0 +1,111 @@
+#---------------------------------------------------------------------
+# 
+# OpenOceanMap - An Open Source GIS tool for performing interviews
+# to obtain socio-economic data using spatial information.
+# 
+# Copyright (C) 2007  Ecotrust
+# Copyright (C) 2007  Aaron Racicot
+# 
+#---------------------------------------------------------------------
+# 
+# licensed under the terms of GNU GPL 2
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
+#---------------------------------------------------------------------
+
+
+# PyQt4 includes for python bindings to QT
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+# QGIS bindings for mapping functions
+from qgis.core import *
+from qgis.gui import *
+# Custom Tools
+from interviewstart import *
+#from Tools.polygontool import *
+#from nextpolygon import *
+# UI specific includes
+from interviewstart_ui import Ui_InterviewStart
+# General system includes
+import sys
+
+# Interview object for doing interviews
+class Interview(object):
+  def __init__(self, parent):
+    self.parent = parent
+    self.canvas = parent.canvas
+    self.mainwindow = parent.parent
+    
+    # A place to store polygons we capture
+    self.capturedPolygons = []
+    self.capturedPolygonsPennies = []
+    self.capturedPolygonsRub = []
+
+    # Interview info to write in shapefile
+    self.interviewInfo = []
+    
+    # Reset previous polygons
+    flags = Qt.WindowTitleHint | Qt.WindowSystemMenuHint | Qt.WindowMaximizeButtonHint 
+    wnd = InterviewStartGui(self,flags)
+    wnd.show()
+
+  # End interview dialog
+  def interviewEnd(self):
+      if len(self.capturedPolygons) == 0:
+          capture_string = QString("No Shapes to write...")
+          self.parent.statusbar.showMessage(capture_string)
+      else:
+          capture_string = QString("Writing shapefile...")
+          self.parent.statusbar.showMessage(capture_string)
+          qd=QFileDialog()
+          filter_str = QString("*.shp")
+          f=qd.getSaveFileName(self.mainwindow,QString(),QString(),filter_str)
+          write_string = QString(f)
+          # define fields for feature attributes
+          fields = { 0 : QgsField("interviewer_name", QVariant.String),
+                     1 : QgsField("participant_name", QVariant.String),
+                     2 : QgsField("pennies", QVariant.Int) }
+          
+          # create an instance of vector file writer,
+          # it will create the shapefile. Arguments:
+          # 1. path to new shapefile (will fail if exists already)
+          # 2. encoding of the attributes
+          # 3. field map
+          # 4. geometry type - from WKBTYPE enum
+          # 5. layer's spatial reference (instance of QgsSpatialRefSys)
+          writer = QgsVectorFileWriter(write_string, "UTF-8", fields,
+                                       QGis.WKBPolygon, None)
+          
+          if writer.hasError() != QgsVectorFileWriter.NoError:
+              print "Error when creating shapefile: ", writer.hasError()
+              
+          # add some features
+          for capPolyInd, capPoly in enumerate(self.capturedPolygons):
+              fet = QgsFeature()
+              ret_val = fet.setGeometry(QgsGeometry.fromWkt(capPoly))
+              fet.addAttribute(0, QVariant(self.interviewInfo[0]))
+              fet.addAttribute(1, QVariant(self.interviewInfo[1]))
+              fet.addAttribute(2, QVariant(self.capturedPolygonsPennies[capPolyInd]))
+              writer.addFeature(fet)
+          del writer
+          capture_string = QString("Wrote Shapefile..." + write_string)
+          self.parent.statusbar.showMessage(capture_string)
+
+          # add some features
+          for capPolyRub in self.capturedPolygonsRub:
+            capPolyRub.reset()
+          self.canvas.setMapTool(self.parent.toolZoomIn)
+
