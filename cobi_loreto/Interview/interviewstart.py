e1fcab36f4585f064fcdab8061b444b53c0c12d2
Dane Springmeyer <dane@dbsgeo.com>
2008-06-25 07:07:57 +0000

    modifications for cobi questioneer

--- cobi_loreto/Interview/interviewstart.py
+++ cobi_loreto/Interview/interviewstart.py
@@ -1,4 +1,5 @@
 #---------------------------------------------------------------------
+# -*- coding: latin-1 -*-
 # 
 # OpenOceanMap - An Open Source GIS tool for performing interviews
 # to obtain socio-economic data using spatial information.
@@ -36,69 +37,117 @@ from qgis.gui import *
 # Custom Tools
 from Tools.polygontool import *
 from selectfishery import *
+from fisherswindow import FishersWindowGui
 # UI specific includes
 from interviewstart_ui import Ui_InterviewStart
 # General system includes
 import sys
 
+from Util.common_functions import *
+
 class InterviewStartGui(QDialog, Ui_InterviewStart):
-    def __init__(self, parent, fl):
-        QDialog.__init__(self, parent.mainwindow, fl)
+    def __init__(self, parent, flags):
+        QDialog.__init__(self, parent.mainwindow, flags)
         self.setupUi(self)
         self.parent = parent
 
-    def on_pbnSelectFishery_released(self):
+    def on_pbnNextWindow_released(self):
         interviewInfo2 = self.parent.interviewInfo2
         
-        interviewInfo2.append(["date", self.date_line.text()])
-        interviewInfo2.append(["int1_fname", self.interviewer1_first_name_line.text()])
-        interviewInfo2.append(["int1_lname", self.interviewer1_last_name_line.text()])
-        interviewInfo2.append(["int2_fname", self.interviewer2_first_name_line.text()])
-        interviewInfo2.append(["int2_lname", self.interviewer2_last_name_line.text()])
-        interviewInfo2.append(["fname", self.interviewee_first_name_line.text()])
-        interviewInfo2.append(["lname", self.interviewee_last_name_line.text()])
-        interviewInfo2.append(["age", self.age_line.text()])
-        interviewInfo2.append(["gender", self.gender_comboBox.currentText()])
-        interviewInfo2.append(["years", self.years_line.text()])
-        interviewInfo2.append(["license", self.license_line.text()])
-        interviewInfo2.append(["city", self.city_line.text()])
-        interviewInfo2.append(["user_group", self.user_group_comboBox.currentText()])
-        
-        interviewInfo2.append(["v_homep", self.home_port_line.text()])
-        interviewInfo2.append(["v_license", self.ves_license_line.text()])
-        interviewInfo2.append(["v_len", self.vessel_length_line.text()])
-        interviewInfo2.append(["years_op", self.years_op_line.text()])
-        interviewInfo2.append(["v_yearown", self.years_own_line.text()])
-        
-        interviewInfo2.append(["t_region", self.region_comboBox.currentText()])
-        interviewInfo2.append(["t_ind_trip", self.ind_per_trip_line.text()])
-        interviewInfo2.append(["t_day_year", self.days_per_year_line.text()])
-        interviewInfo2.append(["t_tr_time", self.travel_time_line.text()])
-        interviewInfo2.append(["t_tr_dist", self.travel_distance_line.text()])
+        # interviewee group
+        interviewInfo2.append([u"nombre", self.interviewee_first_name_line.text()])
+        interviewInfo2.append([u"apellido", self.interviewee_last_name_line.text()])
+        interviewInfo2.append([u"edad", self.age_line.text()])
+        interviewInfo2.append([u"genero", self.gender_comboBox.currentText()])
+        interviewInfo2.append([u"ciudad", self.city_line.text()])
 
-        #interviewInfo2 = self.parent.interviewInfo2
-        #interviewInfo2["fname"] = self.interviewee_first_name_line.text()
-        #interviewInfo2["lname"] = self.interviewee_last_name_line.text()
-        #interviewInfo2["age"] = self.age_line.text()
-        #interviewInfo2["gender"] = self.gender_comboBox.currentText()
-        #interviewInfo2["years"] = self.years_line.text()
-        #interviewInfo2["years_op"] = self.years_op_line.text()
-        #interviewInfo2["years_own"] = self.years_own_line.text()
-        #interviewInfo2["license"] = self.license_line.text()
-        #interviewInfo2["city"] = self.city_line.text()
-        #interviewInfo2["user_group"] = self.user_group_comboBox.currentText()
-        #
-        #interviewInfo2["int1_fname"] = self.interviewer1_first_name_line.text()
-        #interviewInfo2["int1_lname"] = self.interviewer1_last_name_line.text()
-        #interviewInfo2["int2_fname"] = self.interviewer2_first_name_line.text()
-        #interviewInfo2["int2_lname"] = self.interviewer2_last_name_line.text()
-        #interviewInfo2["date"] = self.date_line.text()
-        #
-        #interviewInfo2["v_license"] = self.ves_license_line.text()
-        #interviewInfo2["v_yearown"] = self.years_own_line.text()
-        #interviewInfo2["v_len"] = self.vessel_length_line.text()
-        #interviewInfo2["v_homep"] = self.home_port_line.text()
-        #
+        # interviewer group
+        interviewInfo2.append([u"fecha", self.date_line.text()])        
+        interviewInfo2.append([u"int_nombre", self.interviewer_first_name_line.text()])
+        interviewInfo2.append([u"int_apell", self.interviewer_last_name_line.text()])
+        interviewInfo2.append([u"anos", self.years_spinBox.text()])
+
+        # income sources group
+        interviewInfo2.append([u"pesca_com", self.comm_fish_line.text()])
+        interviewInfo2.append([u"pesca_dep", self.comm_sport_line.text()])
+        interviewInfo2.append([u"pesca_pri", self.private_fish_line.text()])
+        interviewInfo2.append([u"ecoturismo", self.ecotourism_line.text()])
+        interviewInfo2.append([u"ciencia", self.cons_science_line.text()])
+        interviewInfo2.append([u"otro", self.other_line.text()])
+        interviewInfo2.append([u"otro_desc", self.define_other_line.text()])
+
+        # relaunch interview
+        #flags = Qt.WindowTitleHint | Qt.WindowSystemMenuHint | Qt.WindowMaximizeButtonHint 
+        #wnd = InterviewStartGui(self,flags)
+        #wnd.show()
+
+
+        if self.comm_fish_line:
+            if not strIsInt(self.comm_fish_line.text()):
+                QMessageBox.warning(self, "Income Error", "Income value must be a number (no decimals)")
+                return
+            else:
+                if not self.comm_fish_line.text() == '0':
+                    self.parent.currentCommFish = True
+
+        if self.comm_sport_line:
+            if not strIsInt(self.comm_sport_line.text()):
+                QMessageBox.warning(self, "Income Error", "Income value must be a number (no decimals)")
+                return
+            else:
+                if not self.comm_sport_line.text() == '0':
+                    self.parent.currentCommSport = True
+
+        if self.private_fish_line:
+            if not strIsInt(self.private_fish_line.text()):
+                QMessageBox.warning(self, "Income Error", "Income value must be a number (no decimals)")
+                return
+            else:
+                if not self.private_fish_line.text() == '0':
+                    self.parent.currentPrivateFish = True
+
+        if self.ecotourism_line:
+            if not strIsInt(self.ecotourism_line.text()):
+                QMessageBox.warning(self, "Income Error", "Incomevalue must be a number (no decimals)")
+                return
+            else:
+                if not self.ecotourism_line.text() == '0':
+                    self.parent.currentEcotourism = True
+
+        if self.cons_science_line:
+            if not strIsInt(self.cons_science_line.text()):
+                QMessageBox.warning(self, "Income Error", "Income value must be a number (no decimals)")
+                return
+            else:
+                if not self.cons_science_line.text() == '0':
+                    self.parent.currentConsScience = True
+
+        if self.other_line:
+            if not strIsInt(self.other_line.text()):
+                QMessageBox.warning(self, "Income Error", "Income value must be a number (no decimals)")
+                return
+            else:
+                if not self.other_line.text() == '0':
+                    self.parent.currentOther = True
+                    if not len(self.define_other_line.text()):
+                        QMessageBox.warning(self, "Income Error", "Please Define the Other Income")
+                        return
+        total = int(self.comm_fish_line.text()) + int(self.comm_sport_line.text()) + int(self.private_fish_line.text()) + int(self.ecotourism_line.text()) + int(self.cons_science_line.text()) + int(self.other_line.text())
+        print total
+        if not int(total) == int(100):
+            QMessageBox.warning(self, "Income Error", "All Values currently total %s: Please confirm they add up to 100." % total)
+            return
+
+        #import pdb
+        #pyqtRemoveInputHook()
+        #pdb.set_trace()
+        # validation example 
+        #if not cur_fishery:
+         #   QMessageBox.warning(self, "Fishery Error", "Please select a fishery")
+           # return 
+        #else:
+          #  self.parent.currentFishery = cur_fishery
+
         #interviewInfo2["t_region"] = self.region_comboBox.currentText()
         #interviewInfo2["t_ind_trip"] = self.ind_per_trip_line.text()
         #interviewInfo2["t_day_year"] = self.days_per_year_line.text()
@@ -108,7 +157,7 @@ class InterviewStartGui(QDialog, Ui_InterviewStart):
         #self.parent.interviewInfo.append(self.line_2.text())
         self.close()
         flags = Qt.WindowTitleHint | Qt.WindowSystemMenuHint | Qt.WindowMaximizeButtonHint 
-        wnd = SelectFisheryGui(self.parent,flags)
+        wnd = FishersWindowGui(self.parent,flags)
         wnd.show()
         #mc = self.parent.canvas      
         #self.p = PolygonTool(mc,self.parent)
@@ -125,6 +174,12 @@ class InterviewStartGui(QDialog, Ui_InterviewStart):
         # stop interview process
         self.parent.parent.interviewInProgress = False
         self.parent.parent.interviewSaveTool = None
+        self.parent.currentCommFish = False
+        self.parent.currentCommSport = False
+        self.parent.currentPrivateFish = False
+        self.parent.currentEcotourism = False
+        self.parent.currentConsScience = False
+        self.parent.currentOther = False
         self.parent.canvas.setMapTool(self.parent.parent.toolZoomIn)
 
     #def nextPolygon(self):
