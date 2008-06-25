c6c90c0ad9a2cc34a0295403d0443f8b5a168f64
Dane Springmeyer <dane@dbsgeo.com>
2008-06-25 07:17:23 +0000

    adding compiled(qm) and translation source

---
+++ cobi_loreto/Main/i18n/oom_es.ts
@@ -0,0 +1,383 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1" language="lo">
+
+<context>
+    <name>Interview</name>
+    <message>
+        <location filename="Interview/interview.py" line="98"/>
+        <source>Save Shapes as</source>
+        <translation>Switching Save as worked!!!</translation>
+    </message>
+    <message>
+        <location filename="Interview/interview.py" line="111"/>
+        <source>Overwriting existing shapefile is not supported: </source>
+        <translation>spanish here for this shapefile was written: </translation>
+    </message>
+</context>
+
+<context>
+    <name>InterviewStart</name>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="14"/>
+        <source>OpenOceanMap - Interview</source>
+        <translation>Inicio</translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="26"/>
+        <source>Trip Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="47"/>
+        <source>Region :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="58"/>
+        <source>San Francisco County and south, including Pillar Point</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="63"/>
+        <source>San Francisco Bay access points to Point Reyes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="68"/>
+        <source>Point Reyes north to Alder Creek, including Bodega Bay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="88"/>
+        <source>Individuals per trip :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="123"/>
+        <source>Average days fishing per year :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="158"/>
+        <source>Travel time from home to fishing location :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="193"/>
+        <source>Travel distance from home to fishing location :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="222"/>
+        <source>Vessel Information</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="243"/>
+        <source>Vessel license :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="278"/>
+        <source>Years operating a boat :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="313"/>
+        <source>Years of ownership :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="348"/>
+        <source>Vessel length :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="383"/>
+        <source>Home Port :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="412"/>
+        <source>Interviewee</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="888"/>
+        <source>First Name :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="923"/>
+        <source>Last Name :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="503"/>
+        <source>Age :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="538"/>
+        <source>Gender :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="546"/>
+        <source>Male</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="551"/>
+        <source>Female</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="571"/>
+        <source>Years of experience :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="606"/>
+        <source>License number :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="641"/>
+        <source>City of residence :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="676"/>
+        <source>Rec. fishing user group :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="684"/>
+        <source>Operator of Com. Pass. Fishing Vessels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="689"/>
+        <source>Private boat rec. anglers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="694"/>
+        <source>Kayak-based anglers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="699"/>
+        <source>Pier and shore anglers</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="713"/>
+        <source>Interviewer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="734"/>
+        <source>Date of interview (mm/dd/yyyy) :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="782"/>
+        <source>Interviewer #1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="867"/>
+        <source>Interviewer #2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="980"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/interviewstart.ui" line="987"/>
+        <source>Select Fishery</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="Main/mainwindow.ui" line="13"/>
+        <source>OpenOceanMap</source>
+        <translation>MapaOceanAbierto</translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="72"/>
+        <source>Leyenda</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="115"/>
+        <source>Mensajes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="148"/>
+        <source>Entrevista</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="154"/>
+        <source>Mapa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="166"/>
+        <source>Utilidades</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="180"/>
+        <source>Alejar zum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="188"/>
+        <source>Desplazar mapa</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Main/mainwindow.ui" line="196"/>
+        <source>Añadir una capa ráster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="210"/>
+        <source>Inicio Entrevista</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Main/mainwindow.ui" line="218"/>
+        <source>Región herramienta</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="226"/>
+        <source>Polygon Tool</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="234"/>
+        <source>Acercar zum</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message encoding="UTF-8">
+        <location filename="Main/mainwindow.ui" line="242"/>
+        <source>Añadir una capa vectorial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Main/mainwindow.ui" line="250"/>
+        <source>Consola de Python</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NextPolygon</name>
+    <message>
+        <location filename="Interview/nextpolygon.ui" line="14"/>
+        <source>OpenOceanMap - Next Polygon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/nextpolygon.ui" line="47"/>
+        <source>Weighting (Pennies)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/nextpolygon.ui" line="99"/>
+        <source>Finished With Fishery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/nextpolygon.ui" line="106"/>
+        <source>Discard Last Shape...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/nextpolygon.ui" line="113"/>
+        <source>More Shapes This Fishery...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PythonWin</name>
+    <message>
+        <location filename="Util/pythoninterp.ui" line="14"/>
+        <source>OpenOceanMap - Python Console</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Util/pythoninterp.ui" line="26"/>
+        <source>&gt;&gt;&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Util/pythoninterp.ui" line="61"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SelectFishery</name>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="14"/>
+        <source>OpenOceanMap - Select Fishery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="35"/>
+        <source>Select Fishery :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="43"/>
+        <source>Chinook Salmon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="48"/>
+        <source>Rockfish/Lingcod Complex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="53"/>
+        <source>California Halibut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="58"/>
+        <source>Dungeness Crab</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="63"/>
+        <source>Abalone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="68"/>
+        <source>Other</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="88"/>
+        <source>Finished With Interview</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="Interview/selectfishery.ui" line="95"/>
+        <source>Start Drawing Shapes</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
