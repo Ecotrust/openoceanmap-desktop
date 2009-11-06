a106edd20ab070b86318619a92758896df770964
Grant Gilron <grant@ecotrust.ca>
2009-11-06 17:53:26 +0000

    Final changes for BC Commercial 1.0

--- oom_bc_comm/Util/pythoninterp_ui.py
+++ oom_bc_comm/Util/pythoninterp_ui.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'pythoninterp.ui'
 #
-# Created: Mon Jan 14 13:54:51 2008
-#      by: PyQt4 UI code generator 4.3.1
+# Created: Wed Oct 28 16:23:43 2009
+#      by: PyQt4 UI code generator 4.4.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,20 +12,26 @@ from PyQt4 import QtCore, QtGui
 class Ui_PythonWin(object):
     def setupUi(self, PythonWin):
         PythonWin.setObjectName("PythonWin")
-        PythonWin.resize(QtCore.QSize(QtCore.QRect(0,0,600,476).size()).expandedTo(PythonWin.minimumSizeHint()))
-
+        PythonWin.resize(600, 476)
         self.label_1 = QtGui.QLabel(PythonWin)
-        self.label_1.setGeometry(QtCore.QRect(10,420,31,21))
+        self.label_1.setGeometry(QtCore.QRect(10, 420, 31, 21))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        self.label_1.setFont(font)
         self.label_1.setObjectName("label_1")
-
         self.line_1 = QtGui.QLineEdit(PythonWin)
         self.line_1.setEnabled(True)
-        self.line_1.setGeometry(QtCore.QRect(60,417,511,22))
+        self.line_1.setGeometry(QtCore.QRect(60, 417, 511, 22))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        self.line_1.setFont(font)
         self.line_1.setObjectName("line_1")
-
         self.pbnFinished = QtGui.QPushButton(PythonWin)
         self.pbnFinished.setEnabled(False)
-        self.pbnFinished.setGeometry(QtCore.QRect(490,440,83,28))
+        self.pbnFinished.setGeometry(QtCore.QRect(490, 440, 83, 28))
+        font = QtGui.QFont()
+        font.setFamily("Tahoma")
+        self.pbnFinished.setFont(font)
         self.pbnFinished.setFocusPolicy(QtCore.Qt.TabFocus)
         self.pbnFinished.setObjectName("pbnFinished")
 
