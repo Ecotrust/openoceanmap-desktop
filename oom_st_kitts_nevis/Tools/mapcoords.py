36614a11b7e778cefcca24c5781a645131475212
Ken Vollmer <vormooth@gmail.com>
2010-03-25 16:36:38 +0000

    use degree symbol in lat/lon

--- oom_st_kitts_nevis/Tools/mapcoords.py
+++ oom_st_kitts_nevis/Tools/mapcoords.py
@@ -64,8 +64,8 @@ class MapCoords(object):
       lat_dir = "S"
     else:
       lat_dir = "N"
-    capture_string = QString('%#f' % abs(mercator_p.y()) + lat_dir + " , " +
-                             '%#f' % abs(mercator_p.x()) + lon_dir)
+    capture_string = QString('%#f' % abs(mercator_p.y()) + '\260' + lat_dir + " , " +
+                             '%#f' % abs(mercator_p.x()) + '\260' + lon_dir)
     #self.statusbar.showMessage(capture_string)
     
     self.latlon.setText(capture_string)
